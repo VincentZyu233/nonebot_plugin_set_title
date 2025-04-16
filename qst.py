696226d57e4b19eed710828fb3792ca92e5e9458
VincentZyu <1830540513@qq.com>
2025-04-17 07:39:33 +0800

    qwq

--- qst.py
+++ qst.py
@@ -1,22 +1,53 @@
+import os
+
 from nonebot import require
 
 require("nonebot_plugin_alconna")
 
-from nonebot.adapters.onebot.v11 import Bot, Event
+from nonebot.adapters.onebot.v11 import Bot, Event, MessageSegment
 from arclet.alconna import Alconna, Option, Args
-from nonebot_plugin_alconna import Alconna, on_alconna, Match, UniMessage, At, CommandMeta
+from nonebot_plugin_alconna import Alconna, on_alconna, Match, UniMessage, At, CommandMeta, Image
+from nonebot import on_command
 
-usage_string = "qst=\"qwq_set_special_title\" \n 用法: qst <头衔:str> [用户:At]" 
 
-qst_command = on_alconna(
+usage_string = "\tqst=\"qwq_set_special_title\" \n\t\t用法: qst <头衔:str> [用户:At]" 
+
+# 第一种用法：qst <touxian:str> (修改自己)
+qst_set_self = on_alconna(
     Alconna(
-        [""], 
-        "qst", 
-        Args["touxian?", str]["target?", At],
+        [""],
+        "qst",
+        Args["touxian", str],
         meta=CommandMeta(
-            description="设置群头衔",
+            description="直接设置自己的群头衔",
+        ),
+    )
+)
+
+@qst_set_self.handle()
+async def qst_set_self_handle(touxian: Match[str], bot: Bot, event: Event):
+    try:
+        await qst_set_self.send("set self")
+        await bot.set_group_special_title(
+            group_id=event.group_id,
+            user_id=event.get_user_id(),
+            special_title=touxian.result,
+            duration=1
+        )
+    except Exception as e:
+        await qst_set_self.send(f"qst failed (self). \n\te = {e}")
+
+
+# 第二种用法：qst <touxian:str> <user:at> (修改指定用户)
+qst_set_other = on_alconna(
+    Alconna(
+        [""],
+        "qst",
+        Args["touxian", str]["target", At],
+        meta=CommandMeta(
+            description="设置指定用户的群头衔",
             usage=usage_string,
-            example="qs 这是一个头衔qwq @VincentZyu",
+            example="\tqs 这是一个头衔qwq @VincentZyu",
             author="VincentZyu",
             fuzzy_match=False,
             raise_exception=True
@@ -24,65 +55,34 @@ qst_command = on_alconna(
     )
 )
 
-
-@qst_command.assign(
-    "touxian"
-)
-async def qst_handle(
-    touxian: Match[str], 
-    target: Match[At],
-    bot: Bot, 
-    event: Event
-):    
-    if not touxian.available:
-        return
-    
-    # target_qqid = target.result.target if target.available else event.get_user_id()
-    target_qqid = event.get_user_id()
-    if target.available:
-        target_qqid = target.result.target
-    
-    qst_command.set_path_arg("touxian", touxian.result)
-    qst_command.set_path_arg("target_qqid", target_qqid)
-    
-    # await qst_command.send(f"[debug] \n\t touxian = {touxian.result} \n\t target = {target_qqid}")
-
+@qst_set_other.handle()
+async def qst_set_other_handle(touxian: Match[str], target: Match[At], bot: Bot, event: Event):
+    target_qqid = target.result.target
     try:
-        # await qst_command.send("[debug] 111")
+        await qst_set_self.send("set other")
         await bot.set_group_special_title(
-            group_id = event.group_id,
-            user_id = target_qqid,
-            special_title = touxian.result,
-            duration = 1
+            group_id=event.group_id,
+            user_id=target_qqid,
+            special_title=touxian.result,
+            duration=1
         )
     except Exception as e:
-        await qst_command.send(f"qst failed. \n\te = {e}")
+        await qst_set_other.send(f"qst failed (other). \n\te = {e}")
 
 
-@qst_command.got_path(
-    "touxian", 
-    prompt=UniMessage.template("{:At(user, $event.get_user_id())} 请输入头衔"),
+
+qst_help_command = on_alconna(
+    Alconna(
+        "qst_help"
+    )
 )
-async def qst_got(
-    touxian: str,
-    bot: Bot, 
-    event: Event
-):
-    # assert touxian
-    if qst_command.get_path_arg("touxian", default=None) is not None:
-        return
 
-    try:
-        # await qst_command.send("[debug] 222")
-        await bot.set_group_special_title(
-            group_id = event.group_id,
-            user_id = event.get_user_id(),
-            # special_title = "foooooooooooooooooooooooqwerty",
-            special_title = touxian,
-            duration = 1
-        )
-    except Exception as e:
-        await qst_command.send(f"qst failed. \n\te = {e}")
-
-
-    
+@qst_help_command.handle()
+async def handle_qst_help():
+    print("on_command: qst_help")
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    img_path = os.path.join(current_dir, "code.png")
+    await qst_help_command.send(
+        MessageSegment.text("source code↓") +
+        MessageSegment.image(img_path)
+    )
